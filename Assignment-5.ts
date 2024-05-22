3ffe0f77ff4c2b7efc2fb402fd02c14585369288
TalhaYaseen075 <muhammadty785@gmail.com>
2024-05-22 08:41:03 +0500

    Function Return Added After Arrow

--- Assignment-5.ts
+++ Assignment-5.ts
@@ -1,25 +1,19 @@
 // Write a program that uses filter to remove all negative numbers from an array of numbers...
 
 let nums1: number [] = [1 , -10 , 19 , -22 , -45 , 23 , 34 , -43 , -1 , -4 , -6 , -32];
-let nums1Result: number [] = nums1.filter((element: number): boolean  => {
-    return element > 0
-});
+let nums1Result: number [] = nums1.filter((element: number): boolean  => element >= 0);
 console.log("nums1Result" , nums1Result);
 
 // Given an array of numbers [1, 2, 3, 4, 5], use the map method to create a new array where each number is multiplied by 2...
 
 let nums2: number [] = [1 , 2 , 3 , 4 , 5];
-let nums2Result: number [] = nums2.map((element: number): number => {
-    return element * 2;
-});
+let nums2Result: number [] = nums2.map((element: number): number => element * 2);
 console.log("nums2Result" , nums2Result);
 
 // Given an array of strings ["apple", "banana", "cherry", "date", "grape"], use the filter method to create a new array containing only the fruits with more than 5 characters...
 
 let fruits: string [] = ["apple" , "banana" , "cherry" , "date" , "grape"];
-let fruitsResult: string [] = fruits.filter((element: string , index: number): boolean => {
-    return element.length > 5;
-});
+let fruitsResult: string [] = fruits.filter((element: string , index: number): boolean => element.length > 5);
 console.log("fruitsResult" , fruitsResult);
 
 // Given an array of names ["Alice", "Bob", "Charlie", "David", "Emily"], use the forEach method to log each name with an exclamation mark at the end, e.g., "Alice!"...
@@ -33,9 +27,7 @@ names.forEach((element: string) => {
 // Given an array of temperatures in Celsius [0, 10, 20, 30, 40], use the map method to create a new array where each temperature is converted to Fahrenheit using the formula (Celsius * 9/5) + 32...
 
 let temperatures: number [] = [0 , 10 , 20 , 30 , 40];
-let newTemperature: number [] = temperatures.map((element: number): number => {
-    return (element * 9 / 5) + 32;
-});
+let newTemperature: number [] = temperatures.map((element: number): number => (element * 9 / 5) + 32);
 console.log("newTemperature" , newTemperature);
 
 // Given an array of numbers [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], use the map and filter methods together to create a new array containing the squares of even numbers...
